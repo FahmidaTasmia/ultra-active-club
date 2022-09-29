291547b8c7809509af21bd9b7b71f1fa62debc17
FahmidaTasmia <tasmia1070@gmail.com>
2022-09-29 20:19:10 +0600

    responsive css added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,15 +3,38 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <nav className='header'>
-           <h3>UltraActive</h3>
-           <div>
-            <a href="#Shop">Shop</a>
-            <a href="#Orders">Orders</a>
-            <a href="#Inventory">Inventory</a>
-            <a href="#About">About</a>
-           </div>
-        </nav>
+        <nav class="navbar navbar-expand-sm ">
+        <div class="container-fluid ">
+          <a class="navbar-brand" href="#">UltraFitness</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fa-solid fa-bars"></i>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav  ms-auto">
+              <li class="nav-item ">
+                <a class="nav-link " aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Blog</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Deals</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link ">Success </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link ">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link ">Register</a>
+              </li>
+            </ul>
+          </div>
+          
+        </div>
+      </nav>
     );
 };
 

3751a04e39b93de5203438b2b1ac49a9769d9cc4
FahmidaTasmia <tasmia1070@gmail.com>
2022-09-29 16:31:11 +0600

    first commit added

---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Blog = () => {
+    return (
+        <div className='question' >
+            <div className='question-one' >
+             <h3>What is React? and how does it work?</h3>
+             
+            </div>
+        </div>
+    );
+};
+
+export default Blog;
